e90e421f052df7f5b90d0b3f86880261c4b6cabc
yimon <yimonsoe.yms@icloud.com>
2025-11-19 14:14:30 +1300

    study app initial commit

---
+++ app_services/src/main/java/com/study/app_services/AppServicesApplication.java
@@ -0,0 +1,13 @@
+package com.study.app_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppServicesApplication.class, args);
+	}
+
+}
---
+++ app_services/src/test/java/com/study/app_services/AppServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.app_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
